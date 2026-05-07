919467f931790ccf0e9fb282f72feb3d95ca375f
Kyle <canepak@oregonstate.edu>
2026-05-06 21:00:52 -0700

    Split page.tsx into component files under src/components.

---
+++ src/components/DatasetPicker.tsx
@@ -0,0 +1,27 @@
+import { FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+import { DatasetMeta } from '@/types/data';
+
+interface DatasetPickerProps {
+  selectedIndex: number;
+  datasetMeta: DatasetMeta[];
+  onSelect: (value: number) => void;
+}
+
+export default function DatasetPicker({ selectedIndex, datasetMeta, onSelect }: DatasetPickerProps) {
+  return (
+    <FormControl fullWidth sx={{ mb: 3 }}>
+      <InputLabel>Select a dataset</InputLabel>
+      <Select
+        value={selectedIndex}
+        label="Select a dataset"
+        onChange={(e) => onSelect(Number(e.target.value))}
+      >
+        {datasetMeta.map((ds, i) => (
+          <MenuItem key={i} value={i}>
+            {ds.title}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+}
